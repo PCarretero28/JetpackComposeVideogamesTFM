d6ecd3f7c2a9ffe16db7789071a9f374a579ccb7
PCarretero28 <120803089+PCarretero28@users.noreply.github.com>
2023-08-31 17:02:48 +0200

    New design for FavGameCardItem

--- app/src/main/java/com/example/jetpackcomposevideogamestfm/ui/screens/FavGamesScreen.kt
+++ app/src/main/java/com/example/jetpackcomposevideogamestfm/ui/screens/FavGamesScreen.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposevideogamestfm.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,13 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,6 +31,8 @@ import com.example.jetpackcomposevideogamestfm.ui.FavGamesViewModel
 import com.example.jetpackcomposevideogamestfm.ui.theme.FavsBackgroundColor
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import coil.compose.rememberAsyncImagePainter
@@ -64,20 +65,31 @@ fun FavGamesBody(gameList: List<Game>) {
         modifier = Modifier
             .background(FavsBackgroundColor)
             .fillMaxSize()
-            .padding(start = 2.dp, end = 2.dp, bottom = 32.dp)
+            .padding(start = 2.dp, end = 2.dp, bottom = 36.dp)
     ) {
         if (gameList.isEmpty()) {
-            Text(
-                text = "Go and add some of your favorite games!",
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleMedium
-            )
+            SearchForFavGames(Modifier.align(Alignment.CenterHorizontally))
         } else {
             FavGamesList(
                 gameList = gameList
             )
-
         }
+    }
+}
+
+@Composable
+fun SearchForFavGames(modifier: Modifier) {
+    Box(modifier = modifier
+        .fillMaxSize()
+    ) {
+        Text(
+            text = "Go and add some of your favorite games!",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleMedium
+        )
+        CircularProgressIndicator(
+            color = Color.LightGray,
+            strokeWidth = 8.dp)
     }
 }
 
@@ -86,12 +98,8 @@ fun FavGamesList(
     gameList: List<Game>
 ) {
     LazyColumn {
-        items(gameList.chunked(2)) { chunkedGames ->
-            LazyRow {
-                items(chunkedGames) { game ->
-                    FavGameCardItem(juego = game)
-                }
-            }
+        items(gameList.size) {
+            FavGameCardItem(juego = gameList[it])
         }
     }
 }
@@ -100,25 +108,29 @@ fun FavGamesList(
 @Composable
 fun FavGameCardItem(juego: Game) {
 
-   val metacriticColor = when (juego.metacritic) {
+    val metacriticColor = when (juego.metacritic) {
         in 94..Int.MAX_VALUE -> color1
         in 85..93 -> color2
         in 71..84 -> color3
         in 61..70 -> color4
         in 51..60 -> color5
+        in 0 ..1-> Color.White
         else -> color6
-   }
+    }
+
 
     Card(
-        onClick = {},
+        onClick = {
+                  //Fav Game Details
+        },
         modifier = Modifier
             .padding(4.dp)
-            .heightIn(max = 200.dp, min = 120.dp)
-            .widthIn(max = 200.dp, min = 120.dp),
+            .heightIn(max = 100.dp, min = 70.dp)
+            .fillMaxWidth(),
         elevation = 8.dp,
         shape = RoundedCornerShape(8.dp)
     ) {
-        Column(
+        Row(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MainCardColor)
@@ -126,7 +138,7 @@ fun FavGameCardItem(juego: Game) {
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(3f)
+                    .weight(2f)
             ) {
                 Image(
                     painter = rememberAsyncImagePainter(model = juego.background_image),
@@ -151,14 +163,25 @@ fun FavGameCardItem(juego: Game) {
                     Text(text = juego.released.toString(), color = TextColor)
                     Spacer(modifier = Modifier.weight(1F))
 
-                    Text(
-                        text = juego.metacritic.toString(),
-                        color = metacriticColor,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier
-                            .background(MainCardColor)
-                            .padding(2.dp)
-                    )
+                    if (juego.metacritic != 0){
+                        Text(
+                            text = juego.metacritic.toString(),
+                            color = metacriticColor,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .background(MainCardColor)
+                                .padding(2.dp)
+                        )
+                    }else{
+                        Text(
+                            text = "X",
+                            color = metacriticColor,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .background(MainCardColor)
+                                .padding(2.dp)
+                        )
+                    }
                 }
             }
         }
